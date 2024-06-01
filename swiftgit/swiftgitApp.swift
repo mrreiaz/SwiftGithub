3711dab3517b4c2a935fc162b6610ba284562338
mrreiaz <reiazbubt@gmail.com>
2024-06-01 10:06:23 +0900

    Initial Commit

---
+++ swiftgit/swiftgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftgitApp.swift
+//  swiftgit
+//
+//  Created by Abdul Halim Reiaz on 2024/06/01.
+//
+
+import SwiftUI
+
+@main
+struct swiftgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
