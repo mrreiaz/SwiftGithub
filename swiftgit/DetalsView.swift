cc4791567c17355170c9267e7b38cdaa35737e5d
mrreiaz <reiazbubt@gmail.com>
2024-06-02 16:09:23 +0900

    update hike zoomd app

---
+++ swiftgit/DetalsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetalsView.swift
+//  swiftgit
+//
+//  Created by Abdul Halim Reiaz on 2024/06/02.
+//
+
+import SwiftUI
+
+struct DetalsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetalsView()
+}
---
+++ swiftgit/Models/Hike.swift
@@ -0,0 +1,8 @@
+//
+//  Hike.swift
+//  swiftgit
+//
+//  Created by Abdul Halim Reiaz on 2024/06/01.
+//
+
+import Foundation
